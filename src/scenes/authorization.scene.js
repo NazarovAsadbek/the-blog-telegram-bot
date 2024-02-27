65e5fb1e74531955952b6cd7104795794afcee72
Sardor <sardorergashev977@gmail.com>
2024-02-27 20:05:32 +0500

    fix: problems with authorization

--- src/scenes/authorization.scene.js
+++ src/scenes/authorization.scene.js
@@ -1,10 +1,12 @@
 import { Scenes } from 'telegraf';
+import axios from 'axios';
 
 class AuthorizationScene {
   constructor() {
     // const sceneSteps = []
     this.sceneSteps = [
       this.firstStep.bind(this),
+      this.secondStep.bind(this),
     ];
 
     // const scene = []
@@ -13,22 +15,50 @@ class AuthorizationScene {
       ...this.sceneSteps,
     );
 
-    this.scene.enter((ctx) => ctx.reply('Введите логин 123213213213'));
+    this.scene.enter((ctx) => ctx.reply('Введите логин'));
   }
 
   async firstStep(ctx) {
     try {
       const message = ctx?.message?.text ?? '';
+      ctx.scene.state.username = message;
+
       if (message === '/start') {
+        ctx.scene.state.username = null;
         await ctx.scene.leave();
-        await ctx.scene.enter('authorization_scene');
-      } else {
-        ctx.reply('Введите пароль');
+      }
+      ctx.reply('Введите пароль');
+      return ctx.wizard.next();
+    } catch (e) {
+      ctx.scene.state = {};
+      ctx.reply('Упс... Произошла какая-та ошибка');
+      await ctx.scene.leave();
+      return null;
+    }
+  }
+
+  async secondStep(ctx) {
+    try {
+      const message = ctx?.message?.text ?? '';
+      ctx.scene.state.password = message;
+      if (ctx.scene.state.username !== '/start' && ctx.scene.state.password !== '/start') {
+        const formData = {
+          user: {
+            username: ctx.scene.state.username,
+            email: `test${new Date().getTime()}@mail.ru`,
+            password: ctx.scene.state.password,
+          },
+        };
+        const res = await axios.post('http://155.133.23.97:3003/users', formData);
+        if (res.status < 400) {
+          ctx.reply('Вы успешно авторизировались!');
+        }
         await ctx.scene.leave();
       }
     } catch (e) {
+      ctx.scene.state = {};
       ctx.reply('Упс... Произошла какая-та ошибка');
-      console.error(e);
+      await ctx.scene.leave();
     }
   }
 
